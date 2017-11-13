7beb28ef13eeae6afe83da3f1e85b3ed0a530749
erickstm87 <erickstm87@github.com>
2017-11-12 21:22:27 -0700

    state and condition

--- timer/app/index.js
+++ timer/app/index.js
@@ -1,1 +1,37 @@
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('app'));
+
+class PomodoroTimer extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            timeElapsed: 0
+        };
+    }
+
+    totalTime(arg1, arg2){
+        return arg1 + arg2;
+    }
+
+    componentDidMount(){
+        console.log(new Date());
+        this.interval = setInterval(this.elapseTime.bind(this),1000);
+        this.setState({start: new Date()});
+    }
+
+    elapseTime(){
+        //how much time has elapsed
+        var currentDate = new Date();
+        console.log(this.state.start);
+        //var timeElapsed = Current time - start time
+    }
+
+    render() {
+        return(
+            <div>This timer runs for {this.props.workingTime} minutes, followed by a rest of {this.props.restingTime}, for a total of {this.totalTime(this.props.workingTime, this.props.restingTime)}. <br />There are {this.state.timeElapsed} elapsed seconds</div>
+        )
+    }
+}
+
+ReactDOM.render(
+  <PomodoroTimer workingTime={25} restingTime={5}/>,
+  document.getElementById('app')
+);
--- timer/build/index.js
+++ timer/build/index.js
@@ -1,5 +1,46 @@
-ReactDOM.render(React.createElement(
-  'h1',
-  null,
-  'Hello'
-), document.getElementById('app'));
+
+class PomodoroTimer extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            timeElapsed: 0
+        };
+    }
+
+    totalTime(arg1, arg2) {
+        return arg1 + arg2;
+    }
+
+    componentDidMount() {
+        console.log(new Date());
+        this.interval = setInterval(this.elapseTime.bind(this), 1000);
+        this.setState({ start: new Date() });
+    }
+
+    elapseTime() {
+        //how much time has elapsed
+        var currentDate = new Date();
+        console.log(this.state.start);
+        //var timeElapsed = Current time - start time
+    }
+
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            'This timer runs for ',
+            this.props.workingTime,
+            ' minutes, followed by a rest of ',
+            this.props.restingTime,
+            ', for a total of ',
+            this.totalTime(this.props.workingTime, this.props.restingTime),
+            '. ',
+            React.createElement('br', null),
+            'There are ',
+            this.state.timeElapsed,
+            ' elapsed seconds'
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(PomodoroTimer, { workingTime: 25, restingTime: 5 }), document.getElementById('app'));
