28f2b03d85f1877cc0bf7d9938ce13d551b81e5c
erickstm87 <erickstm87@github.com>
2017-11-28 20:29:09 -0700

    fixed results

--- product-search/app/index.js
+++ product-search/app/index.js
@@ -2,13 +2,19 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const product = require('./products.js');
 
-// class Results extends React.Component{
-//     render(){
-//       return(
-//
-//       )
-//     }
-// }
+class Results extends React.Component{
+    render(){
+      return(
+        <div className="results">
+         <div className="in-stock">
+             <h2><a href="#">Toothpaste</a></h2>
+             <p>$2.99</p>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco labor</p>
+         </div>
+         </div>
+      )
+    }
+}
 
 class SearchBar extends React.Component {
 
@@ -36,15 +42,15 @@ class Search extends React.Component {
     }
 
     handleQuery(query){
-        console.log(query);
-        //this.setState({'query': })
+        // console.log(query);
+        this.setState({'query': query})
     }
 
     render() {
         return(
             <div className="search">
                 <SearchBar onQuery={this.handleQuery.bind(this)}/>
-                //<Results />
+                <Results />
                 </div>
 
         )
